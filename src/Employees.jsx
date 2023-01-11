4c5c9ce901230935c6aaad54e88939dc7a73585d
Gabriel Valdivia <42250290+GabeValdivia@users.noreply.github.com>
2023-01-11 05:53:57 +0000

    Added dropdown for team selection. Began to setState & write a function for handling team state changes.

--- src/Employees.jsx
+++ src/Employees.jsx
@@ -4,6 +4,8 @@ import maleProfile from './images/maleProfile.jpg';
 
 const Employees = () => {
 
+  const [selectedTeam, setTeam] = useState("TeamB");
+
   const [employees, setEmployees] = useState([{
     id: 1,
     fullName: "Bob Jones",
@@ -87,20 +89,35 @@ const Employees = () => {
     designation: "Graphic Designer",
     gender: "male",
     teamName: "TeamD"
-  }])
+  }]);
+
+  function handleTeamSelectionChange(event){
+    console.log(event.target.value);
+    setTeam(event.target.value);
+  }
 
   return (
     <main className="container">
+      <div class="row justify-content-center mt-3 mb-3">
+        <div class="col-6">
+          <select className='form-select form-select-lg' value={selectedTeam} onChange={handleTeamSelectionChange}>
+            <option value="TeamA">Team A</option>
+            <option value="TeamB">Team B</option>
+            <option value="TeamC">Team C</option>
+            <option value="TeamD">Team D</option>
+          </select>
+        </div>
+      </div>
       <div class="row justify-content-center mt-3 mb-3">
         <div class="col-8">
           <div className='card-collection'>
             {
               employees.map((employee) => (
                 <div id="{employee.id}" className='card m-2' style={{ cursor: "pointer" }}>
-                  
-                  {(employee.gender === 'male')?<img src={maleProfile} className='card-img-top' /> 
-                                              :<img src={femaleProfile} className='card-img-top' />}
-                  
+
+                  {(employee.gender === 'male') ? <img src={maleProfile} className='card-img-top' />
+                    : <img src={femaleProfile} className='card-img-top' />}
+
                   <div className='card-body'>
                     <h5 className='card-title'>Full Name: {employee.fullName}</h5>
                     <p className='card-text'><strong>Designation:</strong> {employee.designation}</p>
