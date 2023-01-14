2ca06b654f54f5243768417764d0f600428d5359
Gabriel Valdivia <42250290+GabeValdivia@users.noreply.github.com>
2023-01-14 22:04:25 +0000

    Move teams dropdown selector into it's own component

--- src/Employees.jsx
+++ src/Employees.jsx
@@ -1,6 +1,8 @@
 
+import Teams from './Teams';
 import femaleProfile from './images/femaleProfile.jpg';
 import maleProfile from './images/maleProfile.jpg';
+
 
 const Employees = ({ employees, selectedTeam, handleEmployeeCardClick, handleTeamSelectionChange }) => {
 
@@ -8,12 +10,7 @@ const Employees = ({ employees, selectedTeam, handleEmployeeCardClick, handleTea
     <main className="container">
       <div className="row justify-content-center mt-3 mb-3">
         <div className="col-6">
-          <select className='form-select form-select-lg' value={selectedTeam} onChange={handleTeamSelectionChange}>
-            <option value="TeamA">Team A</option>
-            <option value="TeamB">Team B</option>
-            <option value="TeamC">Team C</option>
-            <option value="TeamD">Team D</option>
-          </select>
+          < Teams selectedTeam={selectedTeam} handleTeamSelectionChange={handleTeamSelectionChange} />
         </div>
       </div>
       <div className="row justify-content-center mt-3 mb-3">
