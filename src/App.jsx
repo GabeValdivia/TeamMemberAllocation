c41a56d8da898807dc6542b1ed736af638a3edc9
Gabriel Valdivia <42250290+GabeValdivia@users.noreply.github.com>
2023-01-14 04:51:24 +0000

    Create 404 Not found component & add route.

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Header from './Header';
 import Employees from './Employees';
 import Footer from './Footer';
 import GroupedTeamMembers from './GroupedTeamMembers';
+import NotFound from './NotFound';
 import Nav from './Nav';
 import { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
@@ -137,7 +138,8 @@ function App() {
               />}>
           </Route>
           <Route path="/GroupedTeamMembers" element={<GroupedTeamMembers />}>
-
+          </Route>
+          <Route path="*" element={<NotFound />}>
           </Route>
         </Routes>
         <Footer />
